2c74f436363a107619c5f3f811ef1bae345dbedc
Phi Long Nguyen <kapustacab@gmail.com>
2018-01-03 20:50:34 +0100

    Service project with Dapper added

---
+++ BSRBankingService/Banking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BSRBankingService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Banking" in both code and config file together.
+    public class Banking : IBanking
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
